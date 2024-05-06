c69ada1f264897504e65facf126b0d2fc60d6eee
Kerbaul Loic <loic.kerbaul@etu.univ-orleans.fr>
2024-05-06 20:29:15 +0200

    Ajout d'activités

--- app/src/main/java/com/example/projet_mobile/AjoutCollaborateurs.java
+++ app/src/main/java/com/example/projet_mobile/AjoutCollaborateurs.java
@@ -2,31 +2,33 @@ package com.example.projet_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class EditDessin extends AppCompatActivity {
+public class AjoutCollaborateurs extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_dessin);
+        setContentView(R.layout.activity_ajout_collaborateurs);
 
         ((Button)findViewById(R.id.retour_arriere)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(EditDessin.this, SignedUpUserPage.class);
+                Intent i = new Intent(AjoutCollaborateurs.this, EditDessin.class);
                 startActivity(i);
             }
         });
 
-        ((Button)findViewById(R.id.ajout_collaborateurs)).setOnClickListener(new View.OnClickListener() {
+        ((Button)findViewById(R.id.valider_ajout)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(EditDessin.this, AjoutCollaborateurs.class);
-                startActivity(i);
+                Toast.makeText(AjoutCollaborateurs.this,"Le nouveau collaborateur a été ajouté", Toast.LENGTH_SHORT).show();
             }
         });
     }
